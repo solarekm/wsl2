3765f3a2730c3718036973bee883b9963ccd2836
Michal Solarek <michal.solarek@outlook.com>
2024-03-04 13:58:07 +0100

    Fixing minor bugs and adding some new functionality

--- wsl2_basic_config/config.sh
+++ wsl2_basic_config/config.sh
@@ -17,8 +17,6 @@ if [ -f "$bashrc_extra" ]; then
   fi
 fi
 
-source ~/.bashrc
-
 # File with package names
 req_packages="req_packages"
 py_libraries="py_libraries"
@@ -50,30 +48,30 @@ done < "$py_libraries"
 # Function to install AWS CLI
 install_aws_cli() {
   if ! command -v aws &> /dev/null; then
-    echo "Installing AWS CLI..."
+    echo "\e[32mInstalling AWS CLI...\e[0m"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
     unzip -q /tmp/awscliv2.zip -d /tmp
     sudo /tmp/aws/install
     rm -rf /tmp/aws /tmp/awscliv2.zip
   fi
-  aws --version
+  echo "$(tput setaf 4)$(aws --version)$(tput sgr0)"
 }
 
 # Function to install Session Manager plugin
 install_session_manager_plugin() {
   if ! command -v session-manager-plugin &> /dev/null; then
-    echo "Installing Session Manager plugin..."
+    echo "\e[32mInstalling Session Manager plugin...\e[0m"
     curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "/tmp/session-manager-plugin.deb"
     sudo dpkg -i /tmp/session-manager-plugin.deb
     rm -f /tmp/session-manager-plugin.deb
   fi
-  session-manager-plugin
+  echo "$(tput setaf 4)$(session-manager-plugin)$(tput sgr0)"
 }
 
 # Function to install Docker
 install_docker() {
   if ! command -v docker &> /dev/null; then
-    echo "Installing Docker..."
+    echo "\e[32mInstalling Docker...\e[0m"
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
@@ -85,9 +83,8 @@ install_docker() {
     sudo usermod -aG docker "$USER"
     sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
     sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-    echo "Docker installed. Please log out and log back in for the group change to take effect."
   fi
-  docker --version
+  echo "$(tput setaf 4)$(docker --version)$(tput sgr0)"
 }
 
 # Function to install Terraform
@@ -99,12 +96,43 @@ install_terraform() {
     sudo apt-get update
     sudo apt-get install -y terraform
   fi
-  terraform --version
+  echo "$(tput setaf 4)$(terraform --version)$(tput sgr0)"
+}
+
+# Function to configure git
+git_configuration() {
+  # Checking whether user data is already set up
+  if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
+      echo "No user data configuration in GIT."
+      read -p "Enter your name: " name
+      read -p "Enter your last name: " last_name
+      read -p "Enter your e-mail address: " email
+
+      # Configuring user data in GIT
+      git config --global user.name "$name $last_name"
+      git config --global user.email "$email"
+
+      echo -e "\e[34monfiguration complete. The user data in GIT has been updated:\e[0m"
+      echo "$(tput setaf 4)$(git config --global --list | grep user)$(tput sgr0)"
+  else
+      echo -e "\e[34mThe user data in GIT is already configured:\e[0m"
+      echo "$(tput setaf 4)$(git config --global --list | grep user)$(tput sgr0)"
+  fi
 }
 
 install_aws_cli
 install_session_manager_plugin
 install_docker
 # install_terraform
+git_configuration
 
-git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+# Cloning the repository "Terraform version manager"
+if [ -d ~/.tfenv ]; then
+  echo -e "\e[34mRepository Terraform version manager already exists.\e[0m"
+else
+  git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+fi
+
+echo
+echo -e "\e[31mThe basic configuration of WSL2 is now complete.\e[0m"
+echo -e "\e[31mYou should restart WSL2 using PowerShell as administrator and use the command 'wsl --shutdown'.\e[0m"
